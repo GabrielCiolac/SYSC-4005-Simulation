2b7fac2dfa9fcbdc8c7199c8ffff42337679b38c
Gabriel Ciolac <gabrielciolac@pop-os.localdomain>
2021-03-12 15:17:25 -0500

    Calculated values

--- Data-CSV/ln-prop-calulator.py
+++ Data-CSV/ln-prop-calulator.py
@@ -1,25 +1,31 @@
 import math
 
-fName = input('Enter File Name:')
-with open(fName,'r') as f:
-    lines = f.read()
-    f.close()
+
+while True:
+    fName = input('Enter File Name:')
+    with open(fName,'r') as f:
+        lines = f.read()
+        f.close()
 
 
-values = lines.splitlines()
+    values = lines.splitlines()
 
-sum = 0
+    sum = 0
 
-for val in values[1:-1]:
-    sum = sum + math.log(float(val))
+    for val in values[1:-1]:
+        sum = sum + math.log(float(val))
 
-avg = sum / (len(values) - 2)
+    avg = sum / (len(values) - 2)
 
-sum_of_squares = 0
-for val in values[1:-1]:
-    sum_of_squares = sum + pow((float(val) - avg),2)
+    sum_of_squares = 0
+    for val in values[1:-1]:
+        sum_of_squares = sum + pow((float(val) - avg),2)
 
-sigma = sum_of_squares / (len(values) - 1)
+    sigma = sum_of_squares / (len(values) - 1)
 
-print("Mu: "+str(avg))
-print("Sigma: "+str(sigma))
+    with open('log-normal-properties.txt','a') as f:
+        f.write(fName+'\n')
+        f.write("\tMu: "+str(avg)+'\n')
+        f.write("\tSigma: "+str(sigma)+'\n')
+    print("Mu: "+str(avg))
+    print("Sigma: "+str(sigma))
