8386c9a35e530784186107b605b01ed6a7b1a614
Gabriel Ciolac <gabrielciolac@pop-os.localdomain>
2021-03-12 14:53:47 -0500

    Data Plots

---
+++ Data-CSV/ln-prop-calulator.py
@@ -0,0 +1,25 @@
+import math
+
+fName = input('Enter File Name:')
+with open(fName,'r') as f:
+    lines = f.read()
+    f.close()
+
+
+values = lines.splitlines()
+
+sum = 0
+
+for val in values[1:-1]:
+    sum = sum + math.log(float(val))
+
+avg = sum / (len(values) - 2)
+
+sum_of_squares = 0
+for val in values[1:-1]:
+    sum_of_squares = sum + pow((float(val) - avg),2)
+
+sigma = sum_of_squares / (len(values) - 1)
+
+print("Mu: "+str(avg))
+print("Sigma: "+str(sigma))
