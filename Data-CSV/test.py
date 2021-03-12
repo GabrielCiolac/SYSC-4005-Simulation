a00bc057d2ef9ee03db2a5ce1303deeb9f0706e1
Dom123k <DominicKocjan@cmail.carleton.ca>
2021-03-12 13:52:27 -0500

    testing python script
    To get histogram plots

---
+++ Data-CSV/test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import math 
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+servinsp1 = pd.read_csv('servinsp1.csv')
+servinsp1_array = np.asarray(servinsp1['time'])
+
+
+# In[3]:
+
+
+servinsp1_array
+
+
+# In[16]:
+
+
+bin_count = math.sqrt(servinsp1_array.size)
+bin_count_min = math.ceil(bin_count)
+bin_count_max =servinsp1_array.size/5
+bin_count_max = math.ceil(bin_count_max)
+
+
+# In[19]:
+
+
+bin_count_max
+
+
+# In[22]:
+
+
+sns.displot(servinsp1_array,bins =  25,kde=True)
+
+
+# In[94]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
